d9df48df20cc704c885229d77424d30da5d0d5e0
Andrés Felipe Pardo Mesa <a.pardo6162@gmail.com>
2018-02-21 17:04:25 -0500

    Avance: 	-Metodo agregar orden sin prueba

--- src/main/resources/static/js/OrdersController.js
+++ src/main/resources/static/js/OrdersController.js
@@ -1,22 +1,22 @@
 var orders = {
-    "order_id": 1,
-        "table_id": 1,
-            "products": [{
-                "product": "PIZZA",
-                "quantity": 3,
-                "price": "$15.000"
-            },
-            {
-                "product": "HAMBURGER",
-                "quantity": 1,
-                "price": "$12.300"
-            }
-            ]
+    "table_id": 1,
+    "products": [{
+        "product": "PIZZA",
+        "quantity": 3,
+        "price": "$15.000"
+        },
+        {
+        "product": "HAMBURGER",
+        "quantity": 1,
+        "price": "$12.300"
+        }
+        ]
 }
 
-addOrders();
+createOrder();
 
 function addOrders(){
+    createOrder();
     for (i=0;i < orders.length;i++){
         addOrder(i);
     }
@@ -35,7 +35,18 @@ function addOrder(i){
     Product.innerHTML= orders[i].order.Product;
 }
 
-function createTable() {
+function createOrder() {
     var body = document.getElementsByTagName('body')[0];
     var order = document.createElement('table');
+    for (i=0;i< orders.products.length;i++){
+        var newOrder = order.insertRow();
+        var Price = newOrder.insertCell(0);
+        var Quantity = newOrder.insertCell(0);
+        var Product = newOrder.insertCell(0);
+        Price.innerHTML= "$"+orders.products[i].price;
+        Quantity.innerHTML= orders.products[i].quantity
+        Product.innerHTML= orders.products[i].product
+    }
+    
+    
 }
